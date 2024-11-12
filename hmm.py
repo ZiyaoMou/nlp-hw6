1ba32f6893444412ff9b33312115a7a7fcedbc8b
zmou1 <15905802605@163.com>
2024-11-12 00:04:43 -0500

    feat: crf

--- hmm.py
+++ hmm.py
@@ -4,8 +4,9 @@
 # Starter code for Hidden Markov Models.
 
 from __future__ import annotations
+from ast import Index
 import logging
-from math import inf, log, exp
+from math import inf, log, exp, prod
 from pathlib import Path
 from typing import Callable, List, Optional, cast
 from typeguard import typechecked
@@ -163,14 +164,13 @@ class HiddenMarkovModel:
         # Don't forget to respect the settings self.unigram and λ.
         # See the init_params() method for a discussion of self.A in the
         # unigram case.
+        
         self.A_counts += λ
-        self.A = self.A_counts / self.A_counts.sum(dim=1, keepdim=True)
-        self.A[self.eos_t, :] = 0 
-        self.A[:, self.bos_t] = 0
         if self.unigram:
-            self.A = self.A.sum(dim=0, keepdim=True) / self.k
-
-        return 
+            self.A = self.A_counts.sum(dim=0, keepdim=True) / self.A_counts.sum()
+            self.A = self.A.repeat(self.k, 1)
+        else:
+            self.A = self.A_counts / self.A_counts.sum(dim=1, keepdim=True)
 
     def _zero_counts(self):
         """Set the expected counts to 0.  
@@ -290,65 +290,68 @@ class HiddenMarkovModel:
     @typechecked
     def forward_pass(self, isent: IntegerizedSentence) -> TorchScalar:
         """Run the forward algorithm from the handout on a tagged, untagged, 
-        or partially tagged sentence.  Return log Z (the log of the forward
-        probability) as a TorchScalar.  If the sentence is not fully tagged, the 
+        or partially tagged sentence. Return log Z (the log of the forward
+        probability) as a TorchScalar. If the sentence is not fully tagged, the 
         forward probability will marginalize over all possible tags.  
         
         As a side effect, remember the alpha probabilities and log_Z
         (store some representation of them into attributes of self)
         so that they can subsequently be used by the backward pass."""
-        
-        # The "nice" way to construct the sequence of vectors alpha[0],
-        # alpha[1], ...  is by appending to a List[Tensor] at each step.
-        # But to better match the notation in the handout, we'll instead
-        # preallocate a list alpha of length n+2 so that we can assign 
-        # directly to each alpha[j] in turn.
-        n = len(isent) - 2  # number of words in the sentence (excluding BOS and EOS)
-        alpha = [torch.full((self.k,), -inf) for _ in range(n+2)]   
-        alpha[0] = self.eye[self.bos_t]  # vector that is one-hot at BOS_TAG
-        for m in range(1, len(isent) - 1):
-            word = isent[m][0]
-            tag = isent[m][1]
-            if tag is not None:
-                print(alpha[m-1], torch.log(self.A[:, tag]), torch.log(self.B[:, word]))
-                log_sum_exp = torch.logsumexp(alpha[m-1] + torch.log(self.A[:, tag]) + torch.log(self.B[tag, word]), dim=0)
-                alpha[m][tag] = log_sum_exp
-            else:
-                alpha[m] = torch.logsumexp(alpha[m-1].unsqueeze(1) + torch.log(self.A) + torch.log(self.B[:, word]).unsqueeze(0), dim=0)
-        log_Z = torch.logsumexp(alpha[-2] + torch.log(self.A[:, self.eos_t]), dim=0)
-        alpha[n + 1][self.eos_t] = log_Z
+
+        # Initialize alpha with -inf for all tags, except BOS_TAG
+        n = len(isent) - 2  # length of sentence without BOS and EOS
+        alpha = torch.full((len(isent), self.k), -float('inf'))
+        alpha[0][self.bos_t] = 0  # BOS_TAG starts with log-prob of 0
+
+        # Forward algorithm for each position in the sentence
+        for j in range(1, n + 1):
+            word = isent[j][0]
+            alpha[j] = torch.logsumexp(
+                alpha[j - 1].unsqueeze(1) + torch.log(self.A) + torch.log(self.B[:, word]).unsqueeze(0),
+                dim=0)
+
+        # Final step for EOS tag
+        alpha[n + 1][self.eos_t] = torch.logsumexp(
+            alpha[n] + torch.log(self.A[:, self.eos_t]), dim=0
+        )
+        # Return log Z, the log-probability of observing the given sentence
+        log_Z = alpha[n + 1][self.eos_t]
+        self.alpha = alpha  # Save alpha for use in backward_pass
+        self.log_Z = log_Z  # Save log Z
         return log_Z
 
     @typechecked
     def backward_pass(self, isent: IntegerizedSentence, mult: float = 1) -> TorchScalar:
-        """Run the backward algorithm on a tagged, untagged, or partially tagged sentence.
-        Return log Z (the log of the backward probability).
+        """Run the backwards algorithm from the handout on a tagged, untagged, 
+        or partially tagged sentence.  Return log Z (the log of the backward
+        probability). 
         
-        As a side effect, add the expected transition and emission counts (times `mult`)
-        into self.A_counts and self.B_counts. These depend on the forward probabilities
-        from the forward pass and the backward probabilities from this backward pass."""
+        As a side effect, add the expected transition and emission counts (times
+        mult) into self.A_counts and self.B_counts.  These depend on the alpha
+        values and log Z, which were stored for us (in self) by the forward
+        pass."""
 
-        n = len(isent) - 2  # Exclude BOS and EOS
-        beta = [torch.full((self.k,), -float('inf')) for _ in range(n+1)]
-        beta[-1] = self.eye[self.eos_t]  # Set beta for EOS_TAG to one-hot
-        # Backward pass: Start from end and move towards the beginning
-        for j in range(n, 0, -1):
+        # Pre-allocate beta just as we pre-allocated alpha.
+        beta = [torch.empty(self.k) for _ in isent]
+        beta[-1] = torch.log(self.eye[self.eos_t])  # vector that is one-hot at EOS_TAG
+        # beta[-1][self.eos_t] = 0  # EOS_TAG ends with log-prob of 0
+        # print(beta[-1].shape)
+        n = len(isent) - 2
+        for j in range(n+1, 0, -1):
             word = isent[j][0]
-            tag = isent[j][1]
-            if tag is None:  # Unsupervised case
-                beta[j - 1] = torch.logsumexp(
-                    beta[j].unsqueeze(1) + torch.log(self.A) + torch.log(self.B[:, word]).unsqueeze(0),
-                    dim=0 
-                )
-            else:  # Supervised case
-                beta[j - 1][tag] = torch.logsumexp(
-                    beta[j] + torch.log(self.A[:, tag]) + torch.log(self.B[tag, word]),
-                    dim=0
-                )     
-        # Compute log Z for backward pass by summing paths from BOS_TAG
-        log_Z_backward = torch.logsumexp(torch.log(self.A[self.bos_t]) + beta[0], dim=0)
-
-        return log_Z_backward
+            # beta[j-1] = torch.logsumexp(self.A + self.B[:,word] + beta[j] + self.alpha[j-1].unsqueeze(1), dim=0)
+            for t in range(self.k):
+                try:
+                    beta[j-1][t] = torch.logsumexp(torch.log(self.A[t,:]) + torch.log(self.B[:,isent[j][0]]) + beta[j], dim=0)
+                except IndexError:
+                    beta[j-1][t] = torch.logsumexp(torch.log(self.A[t,:]) + beta[j], dim=0)
+            # print(torch.exp(beta[j-1]))
+        for s in range(self.k): # tag of j
+            for j in range(1, n+1):
+                self.B_counts[s, isent[j][0]] += torch.exp(self.alpha[j][s] + beta[j][s] - self.log_Z) * mult
+                for t in range(self.k): # tag of j-1
+                    self.A_counts[t, s] += torch.exp(self.alpha[j-1][t] + torch.log(self.A[t,s]) + torch.log(self.B[s,isent[j][0]]) + beta[j][s] - self.log_Z) * mult
+        return beta[0][self.bos_t]
 
     def viterbi_tagging(self, sentence: Sentence, corpus: TaggedCorpus) -> Sentence:
         """Find the most probable tagging for the given sentence, according to the
@@ -365,40 +368,35 @@ class HiddenMarkovModel:
         # Sentence object, and that's what downstream methods like eval_tagging
         # will expect.  (Running mypy on your code will check that your code
         # conforms to the type annotations ...)
-        self.A = self.A + 1e-45
-        self.B = self.B + 1e-45
-        
+
         isent = self._integerize_sentence(sentence, corpus)
-        n = len(isent) - 2  # Exclude BOS and EOS
-        viterbi = torch.full((len(isent), self.k), -float('inf'))
-        backpointers = torch.zeros(len(isent), self.k, dtype=torch.long)
-        viterbi[0][self.bos_t] = 0
 
-        for j in range(1, n+1):
-            word = isent[j][0]
-            prob = viterbi[j-1].unsqueeze(1) + torch.log(self.A) + torch.log(self.B[:, word]).unsqueeze(0)
-            max_prob, backpointer = torch.max(prob, dim=0)
-            viterbi[j] = max_prob
-            backpointers[j] = backpointer
+        # See comments in log_forward on preallocation of alpha.
+        alpha        = [torch.empty(self.k)                  for _ in isent]  
+        backpointers = [torch.empty(self.k, dtype=torch.int) for _ in isent]
+        n = len(isent) - 2
+        tags: List[int] = [0 for _ in range(n+2)]    # you'll put your integerized tagging here
+        alpha[0] = torch.log(self.eye[self.bos_t])  # vector that is one-hot at BOS_TAG
+        for j in range(1, len(isent)):
+            for t in range(self.k):
+                # print(alpha[j-1].shape, self.A[:,t].shape, self.B[t, isent[j][0]].shape)
+                try:
+                    prod = alpha[j-1] + torch.log(self.A[:,t]) + torch.log(self.B[t,isent[j][0]])
+                except IndexError:
+                    prod = alpha[j-1] + torch.log(self.A[:,t])
+                # print('0', torch.exp(alpha[j-1]), self.A[:,t], self.B[t,isent[j][0]])
+                # print('1', torch.exp(prod))
+                alpha[j][t] = torch.max(prod)
+                backpointers[j][t] = torch.argmax(prod)
+            # print(torch.exp(alpha[j]))
 
-        p1_eos = torch.log(self.A[:, self.eos_t])
-
-        for i in range(self.k):
-            score = viterbi[n][i] + p1_eos[i]
-            if viterbi[n+1][self.eos_t] < score:
-                viterbi[n+1][self.eos_t] = score
-                backpointers[n+1][self.eos_t] = i
-
-        path = [self.eos_t]
+        # Now follow backpointers to find the most probable tag sequence.
+        tags[n+1] = self.eos_t
         for j in range(n+1, 0, -1):
-            path.append(backpointers[j][path[-1]])
-
-        path.reverse()
-        # raise NotImplementedError   # you fill this in!
-
+            tags[j-1] = backpointers[j][tags[j]]
         # Make a new tagged sentence with the old words and the chosen tags
         # (using self.tagset to deintegerize the chosen tags).
-        return Sentence([(str(self.vocab[isent[i][0]]), str(self.tagset[tag])) for i, tag in enumerate(path)])
+        return Sentence([(word, self.tagset[tags[j]]) for j, (word, tag) in enumerate(sentence)])
 
     def save(self, model_path: Path) -> None:
         logger.info(f"Saving model to {model_path}")
